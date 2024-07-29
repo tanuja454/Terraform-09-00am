399159774657cd7672757aaf1f2f4e4f08f0c17d
tanuja454 <chennapragadalaxminagatanuja@gmail.com>
2024-07-29 12:52:57 +0530

    Update ec2.tf

--- day-3-custom-nw/ec2.tf
+++ day-3-custom-nw/ec2.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "dev" {
     instance_type = var.instance_type
     key_name = var.key_name
     subnet_id = aws_subnet.dev.id
-    associate_public_ip_address = true
-    security_groups = [ aws_security_group.dev.id ]
+    
+  
  
     tags = {
       Name = "dev_ec2"
